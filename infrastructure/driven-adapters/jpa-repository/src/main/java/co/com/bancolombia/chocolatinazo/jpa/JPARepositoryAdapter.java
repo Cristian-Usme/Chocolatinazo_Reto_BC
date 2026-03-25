f95e7fa8c7f93c0836ab2b1d2dfadedf50241e63
Cristian Usme <cristian1213u@gmail.com>
2026-03-24 19:13:01 -0500

    chore: setup project dependencies and initial configuration

---
+++ infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/bancolombia/chocolatinazo/jpa/JPARepositoryAdapter.java
@@ -0,0 +1,12 @@
+package co.com.bancolombia.chocolatinazo.jpa;
+
+//import co.com.bancolombia.chocolatinazo.jpa.helper.AdapterOperations;
+//import org.reactivecommons.utils.ObjectMapper;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public class JPARepositoryAdapter extends AdapterOperations<Object, Object, String, JPARepository>{
+//    public JPARepositoryAdapter(JPARepository repository, ObjectMapper mapper) {
+//        super(repository, mapper, d -> mapper.map(d, Object.class));
+//    }
+//}
---
+++ infrastructure/driven-adapters/jpa-repository/src/test/java/co/com/bancolombia/chocolatinazo/jpa/helper/AdapterOperationsTest.java
@@ -0,0 +1,5 @@
+package co.com.bancolombia.chocolatinazo.jpa.helper;
+
+class AdapterOperationsTest {
+
+}
